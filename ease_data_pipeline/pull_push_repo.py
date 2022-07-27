11f7bc40ea467b38616dba5c480fbe65bdeb33fb
root <root@DESKTOP-8780SOC.localdomain>
2022-07-27 17:43:52 +0200

    push at2022-07-27_05-43-51_PM

--- ease_data_pipeline/pull_push_repo.py
+++ ease_data_pipeline/pull_push_repo.py
@@ -3,7 +3,7 @@ from datetime import date
 from datetime import datetime
 
 file_name = datetime.now().strftime("%Y-%m-%d_%I-%M-%S_%p")
-PATH_OF_GIT_REPO = '/c/Users/user/dags/analytics_app/.git'  # make sure .git folder is properly configured
+PATH_OF_GIT_REPO = r'/c/Users/user/dags/analytics_app/.git'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'push at' + file_name # Commit messge
 
 
@@ -12,10 +12,10 @@ my_branch = 'ease_pipeline' # my branch name, where I'm going to push file
 repo.git.pull('origin', my_branch) # Trying to update local repository, if there are modification on remote Repo 
         
         
-if repo.untracked_files: # Here we firstly check if there are local modifications
-    repo.git.add(A=True)
-    repo.git.commit(m=COMMIT_MESSAGE)
-    repo.git.push('origin', my_branch)
-    print('git push')
-else:
-    print('Noting to commit')
+#if repo.untracked_files: # Here we firstly check if there are local modifications
+repo.git.add(A=True)
+repo.git.commit(m=COMMIT_MESSAGE)
+repo.git.push('origin', my_branch)
+print('git push')
+#else:
+#print('Noting to commit')
