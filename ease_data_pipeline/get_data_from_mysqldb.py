26915254aa9cac001cbfa1cff2ab470785bf405e
cartellefotsing <root@DESKTOP-8780SOC.localdomain>
2022-08-06 15:36:25 +0200

    push at2022-08-06_03-36-22_PM

---
+++ ease_data_pipeline/get_data_from_mysqldb.py
@@ -0,0 +1,14 @@
+
+from sqlalchemy import create_engine
+import pymysql
+import pandas as pd
+
+sqlEngine       = create_engine('mysql+pymysql://cartelle:2021@127.0.0.1/ease', pool_recycle=3600)
+dbConnection    = sqlEngine.connect()
+frame           = pd.read_sql("select * from ease.test_project_trackerdata", dbConnection);
+
+pd.set_option('display.expand_frame_repr', False)
+
+print(frame)
+
+dbConnection.close()
